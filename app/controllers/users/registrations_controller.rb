72b11473bc40117ba880c9c11f544d30a35ce9d0
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-02-04 22:11:42 +0900

    ＃４エラー通知実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,17 +4,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     if resource.save
       sign_up(resource_name, resource)
-      respond_to do |format|
-        format.html { redirect_to after_sign_up_path_for(resource), notice: 'アカウント登録が完了しました' }
-        format.turbo_stream
-      end
+      redirect_to after_sign_up_path_for(resource), notice: 'アカウント登録が完了しました'
     else
       clean_up_passwords(resource)
       set_minimum_password_length
-      respond_to do |format|
-        format.html { render 'devise/sessions/new', status: :unprocessable_entity }
-        format.turbo_stream
-      end
+      flash.now[:alert] = resource.errors.full_messages
+
+      # Turboを無効化し、エラーメッセージを表示させる
+      render 'devise/sessions/new', status: :unprocessable_entity
     end
   end
 end
