20d03536c695856b4ea1da05cd5091256433c2b0
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-02-04 20:32:07 +0900

    Revert "Update Gemfile"
    This reverts commit b9b42f5ca8b9d51eb6b37b75f01a4e5d4e5e1f2e.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: "notes#index" # 仮のトップページ
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
-
+  devise_for :users
   resources :notes, only: [:index, :new, :create, :show] # ここに適宜必要なアクションを追加
 end
