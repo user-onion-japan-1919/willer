c1a2a01588c113e1ffc305ff51f75149cbd98dfd
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-02-04 17:01:08 +0900

    ＃４バリデーション追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "notes#index" # 仮のトップページ
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
+
   resources :notes, only: [:index, :new, :create, :show] # ここに適宜必要なアクションを追加
 end
