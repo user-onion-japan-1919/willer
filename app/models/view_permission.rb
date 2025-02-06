fafe9ba1aabd3c235564ba173be7355f576fcadd
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-02-06 14:08:46 +0900

    一異性まで

--- app/models/view_permission.rb
+++ app/models/view_permission.rb
@@ -1,5 +1,27 @@
 class ViewPermission < ApplicationRecord
   belongs_to :user
 
-  validates :last_name, :last_name_furigana, :first_name, :first_name_furigana, :birthday, :blood_type, presence: true
+  validates :first_name, presence: true
+  validates :first_name_furigana, presence: true
+  validates :last_name, presence: true
+  validates :last_name_furigana, presence: true
+  validates :birthday, presence: true
+  validates :blood_type, presence: true
+
+  validate :unique_combination
+
+  private
+
+  def unique_combination
+    if ViewPermission.exists?(
+      first_name: first_name,
+      first_name_furigana: first_name_furigana,
+      last_name: last_name,
+      last_name_furigana: last_name_furigana,
+      blood_type: blood_type,
+      user_id: user_id # 同じユーザーが重複登録しないように制限
+    )
+      errors.add(:base, 'この閲覧許可対象者はすでに登録されています。')
+    end
+  end
 end
--- app/models/view_request.rb
+++ app/models/view_request.rb
@@ -8,4 +8,21 @@ class ViewRequest < ApplicationRecord
   validates :last_name_furigana, presence: true
   validates :birthday, presence: true
   validates :blood_type, presence: true
+
+  validate :unique_combination
+
+  private
+
+  def unique_combination
+    if ViewRequest.exists?(
+      first_name: first_name,
+      first_name_furigana: first_name_furigana,
+      last_name: last_name,
+      last_name_furigana: last_name_furigana,
+      blood_type: blood_type,
+      parent_id: parent_id
+    )
+      errors.add(:base, '同じ名前・血液型のユーザーがすでに登録されています。')
+    end
+  end
 end
