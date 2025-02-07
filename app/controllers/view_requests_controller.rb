d0015779c850a797f38ea7a3120b7876bde24235
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-02-07 21:35:36 +0900

    カラム編集前

--- app/controllers/view_requests_controller.rb
+++ app/controllers/view_requests_controller.rb
@@ -4,8 +4,8 @@ class ViewRequestsController < ApplicationController
   def create
     Rails.logger.debug "📌 Received Params: #{params.inspect}" # デバッグ用ログ
 
-    # `parent_id` を整数として取得
-    parent_id = params[:view_request][:parent_id].to_i
+    # `parent_id` を取得（フォームから渡された値を使用）
+    parent_id = params[:view_request][:parent_id].present? ? params[:view_request][:parent_id].to_i : nil
     owner = User.find_by(id: parent_id)
 
     unless owner
@@ -35,7 +35,7 @@ class ViewRequestsController < ApplicationController
       first_name: owner.first_name,
       first_name_furigana: owner.first_name_furigana,
       last_name: owner.last_name,
-      last_name_furigana: owner.last_name_furigana,
+      last_name_furigana: owner.last_name_furigana, # ← ここにカンマを追加
       birthday: owner.birthday,
       blood_type: owner.blood_type,
       relationship: params[:view_request][:relationship]
@@ -50,69 +50,12 @@ class ViewRequestsController < ApplicationController
     redirect_to notes_path
   end
 
-  def request_access
-    viewer = current_user # B（閲覧申請者）
-
-    # `parent_id` を整数として取得
-    parent_id = params[:parent_id].to_i
-    view_request = ViewRequest.find_by(user_id: viewer.id, parent_id: parent_id)
-
-    unless view_request
-      flash[:alert] = '閲覧申請情報が見つかりません。'
-      return redirect_to notes_path
-    end
-
-    # `users` テーブルから A（公開者）を検索
-    owner = User.find_by(id: view_request.parent_id)
-
-    unless owner
-      flash[:alert] = '指定された公開者が見つかりません。'
-      return redirect_to notes_path
-    end
-
-    Rails.logger.debug "📌 照合された公開者 (Owner): #{owner.inspect}"
-
-    # **① A（公開者）が B を閲覧許可に登録しているか確認**
-    view_permission = ViewPermission.find_by(
-      user_id: owner.id,
-      first_name: viewer.first_name,
-      first_name_furigana: viewer.first_name_furigana,
-      last_name: viewer.last_name,
-      last_name_furigana: viewer.last_name_furigana,
-      birthday: viewer.birthday,
-      blood_type: viewer.blood_type
-    )
-
-    if view_permission
-      # **A（公開者）の UUID を使って公開ページ URL を取得**
-      public_page_url = public_page_url(uuid: owner.uuid, custom_id: owner.id + 150_150)
-
-      Rails.logger.debug "📌 取得した公開ページURL: #{public_page_url}"
-
-      # **ViewAccess に保存**
-      view_access = ViewAccess.find_or_initialize_by(viewer_id: viewer.id, owner_id: owner.id)
-      view_access.update(
-        public_page_url: public_page_url,
-        access_count: (view_access.access_count || 0) + 1, # アクセス回数を増加
-        last_accessed_at: Time.current
-      )
-
-      if view_access.save
-        Rails.logger.debug "📌 保存された ViewAccess: #{view_access.inspect}"
-        flash[:notice] = "#{owner.first_name} #{owner.last_name} さんの公開ページのURLを取得しました。"
-      else
-        flash[:alert] = "URL の保存に失敗しました: #{view_access.errors.full_messages.join(', ')}"
-      end
-    else
-      flash[:alert] = '閲覧許可が登録されていません。'
-    end
-
-    redirect_to notes_path
-  end
-
   private
 
   def view_request_params
-    params.require(:view_request).permit(:relationship, :parent_id)
+    params.require(:view_request).permit(
+      :first_name, :first_name_furigana, :last_name, :last_name_furigana,
+      :birthday, :blood_type, :relationship, :parent_id
+    )
   end
 end
