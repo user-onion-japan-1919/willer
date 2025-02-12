be66a0c0592cc9f846e7c4d985806b7fa134c685
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-02-12 19:33:02 +0900

    できた！！

--- app/models/view_request.rb
+++ app/models/view_request.rb
@@ -11,11 +11,23 @@ class ViewRequest < ApplicationRecord
 
   validate :unique_combination
 
+  # ✅ `users` テーブルの情報と一致するユーザーを検索
+  def matching_user
+    User.where(
+      first_name: first_name,
+      first_name_furigana: first_name_furigana,
+      last_name: last_name,
+      last_name_furigana: last_name_furigana,
+      birthday: birthday,
+      blood_type: blood_type
+    ).order(:id).first # 先頭のユーザーを取得
+  end
+
   private
 
   # **同じ情報のリクエストが登録されないようにする**
   def unique_combination
-    if ViewRequest.exists?(
+    if ViewRequest.where(
       first_name: first_name,
       first_name_furigana: first_name_furigana,
       last_name: last_name,
@@ -23,7 +35,7 @@ class ViewRequest < ApplicationRecord
       birthday: birthday,
       blood_type: blood_type,
       user_id: user_id
-    )
+    ).where.not(id: id).exists?
       errors.add(:base, '同じ閲覧申請がすでに登録されています。')
     end
   end
