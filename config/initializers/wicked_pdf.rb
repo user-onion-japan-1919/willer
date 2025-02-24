28898c6e84f0234be8b270143043d49bc09d9ead
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-02-24 15:32:13 +0900

    Update wicked_pdf.rb

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,7 @@
 WickedPdf.configure do |config|
-  config.exe_path = '/usr/bin/wkhtmltopdf'
+  if Rails.env.production?
+    config.exe_path = '/usr/bin/wkhtmltopdf' # 本番環境のパス（Render用）
+  else
+    config.exe_path = '/Users/takasakashuta/.rbenv/shims/wkhtmltopdf' # ローカル環境のパス
+  end
 end
