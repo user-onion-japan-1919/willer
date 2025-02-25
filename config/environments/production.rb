f2575ce6f3f9ac340da74a5bff6cfadd55256bdb
user-onion-japan-1919 <shuta.12.27st@gmail.com>
2025-02-25 22:56:57 +0900

    #

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,11 +30,11 @@ Rails.application.configure do
   #config.assets.js_compressor = Uglifier.new(harmony: true)
 
 # JavaScriptの圧縮を有効にする
-config.assets.js_compressor = Uglifier.new(harmony: true)
-
+#config.assets.js_compressor = Uglifier.new(harmony: true, compress: { unused: false })
+config.assets.js_compressor = :terser
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # プリコンパイル時にダイジェストを生成
   config.assets.digest = true
